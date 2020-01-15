dd4cf759ea469a88d31975a67136614609d7b4ea
Lorcan <Lorcan@WAVSOFT>
2020-01-15 11:37:59 +0000

    gsddddd,.lgtrskote

--- wav ocs .com2/login.aspx.vb
+++ wav ocs .com2/login.aspx.vb
@@ -1,6 +1,7 @@
-﻿Public Class login
+﻿Imports System.Data.OleDb
+Public Class login
     Inherits System.Web.UI.Page
-
+    Dim cn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\VisStudioProj\wav web\willairwayvirtual34\willairwayvirtual34\app_data\willairwayvirtualDbv1.accdb")
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
@@ -23,5 +24,35 @@
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         Response.Redirect("Create an Accountaspx.aspx")
+    End Sub
+
+    Private Sub Button1l_Click(sender As Object, e As EventArgs) Handles Button1l.Click
+        'Dim lastName As String
+        Dim query As String
+        query = "select count (*) from tblaccessinfo where uname = ? and pword = ?"
+        Dim result As Integer = 0
+        Using cmd As New OleDbCommand(query, cn)
+            cmd.Parameters.AddWithValue("", TextBox1_uname.Text)
+            cmd.Parameters.AddWithValue("", TextBox2_pword.Text)
+            cn.Open()
+            Session("Uname") = TextBox1_uname.Text
+            result = DirectCast(cmd.ExecuteScalar(), Integer)
+            Dim dr As OleDbDataReader = cmd.ExecuteReader
+            '  If userFound = True Then
+            'FirstName = dr("FName").ToString
+            ' lastName = dr("LName").ToString
+            'Status = dr("status").ToString
+            'totalHours = dr("TotalHours").ToString
+            'lastflight = dr("lastflight").ToString
+            Response.Cookies("Details").Value = TextBox1_uname.Text
+            'End If
+        End Using
+        If result > 0 Then
+
+
+            Response.Redirect("wavocs2aspx.aspx")
+        Else
+            Label2.Text = "YOU DONT EXIST"
+        End If
     End Sub
 End Class
--- wav ocs .com2/wavocs2aspx.aspx.vb
+++ wav ocs .com2/wavocs2aspx.aspx.vb
@@ -5,4 +5,19 @@
 
     End Sub
 
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Response.Redirect("support2.aspx")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Response.Redirect("contact2.aspx")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Response.Redirect("wav ocs1.aspx")
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Response.Redirect("DownloadCenter.aspx")
+    End Sub
 End Class
